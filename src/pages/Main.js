ba91d84e3bd4a28235e9ec49621b5c63a7fe3ba6
Chuks Festus <cutogu@gmail.com>
2018-03-14 16:09:23 +0100

    main page init

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <p>hell</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
