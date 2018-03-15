1753994405e88792d7ca9e9e13328527b333b3aa
Chuks Festus <cutogu@gmail.com>
2018-03-15 09:26:11 +0100

    added topnav, bpttom nav and main content

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import TopNav from "../components/TopNav";
+import MainMenu from "../components/MainMenu";
+
 class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        <p>hell</p>
+        <TopNav />
+        <div className="content bg-grey-light" style={{ height: "100vh" }}>
+          conetnt
+        </div>
+        <MainMenu />
       </div>
     );
   }
